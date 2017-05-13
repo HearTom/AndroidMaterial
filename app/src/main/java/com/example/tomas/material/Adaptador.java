4326c75fc685d8b85abf45c7db1f78186f507a4c
Tomas <tomas_tfha@hotmail.com>
2017-05-13 03:10:15 -0500

    Multiples Layout RecyclerView

--- app/src/main/java/com/example/tomas/material/Adaptador.java
+++ app/src/main/java/com/example/tomas/material/Adaptador.java
@@ -11,25 +11,50 @@ import com.android.volley.toolbox.NetworkImageView;
 
 import java.util.List;
 
-public class Adaptador extends RecyclerView.Adapter<Adaptador.JugadorViewHolder> {
+public class Adaptador extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     List<Jugador> listajugador;
+    private static final int VISTA1 = 1;
+    private static final int VISTA2 = 2;
+
 
     public Adaptador(List<Jugador> listajugador) {
         this.listajugador = listajugador;
     }
 
     @Override
-    public JugadorViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_item, parent, false);
-        return new JugadorViewHolder(v);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        switch (viewType){
+            case VISTA1 :
+                default:
+                View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_item, parent, false);
+                return new JugadorViewHolder(v);
+            case VISTA2:
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_item_template, parent, false);
+                return new Vista2ViewHolder(view);
+        }
+
     }
 
     @Override
-    public void onBindViewHolder(JugadorViewHolder holder, int position) {
-        holder.imgjugador.setImageUrl(listajugador.get(position).getFoto(), VolleySingleton.getInstance().getImageLoader());
-        holder.txtjugador.setText(listajugador.get(position).getNombre());
-        holder.txtequipo.setText(listajugador.get(position).getEquipo());
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        int viewtype = getItemViewType(position);
+        switch (viewtype){
+            case VISTA1:
+                JugadorViewHolder jugadorViewHolder = (JugadorViewHolder) holder;
+                jugadorViewHolder.imgjugador.setImageUrl(listajugador.get(position).getFoto(), VolleySingleton.getInstance().getImageLoader());
+                jugadorViewHolder.txtjugador.setText(listajugador.get(position).getNombre());
+                jugadorViewHolder.txtequipo.setText(listajugador.get(position).getEquipo());
+                break;
+            case VISTA2:
+                Vista2ViewHolder vista2ViewHolder = (Vista2ViewHolder) holder;
+                vista2ViewHolder.imgjugador2.setImageUrl(listajugador.get(position).getFoto(), VolleySingleton.getInstance().getImageLoader());
+                vista2ViewHolder.txtjugador2.setText(listajugador.get(position).getNombre());
+                vista2ViewHolder.txtequipo2.setText(listajugador.get(position).getEquipo());
+                break;
+
+        }
+
     }
 
     @Override
@@ -37,6 +62,10 @@ public class Adaptador extends RecyclerView.Adapter<Adaptador.JugadorViewHolder>
         return listajugador.size();
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        return position % 2 == 0 ? VISTA1 : VISTA2 ;
+    }
 
     public static class JugadorViewHolder extends RecyclerView.ViewHolder {
         NetworkImageView imgjugador;
@@ -51,6 +80,23 @@ public class Adaptador extends RecyclerView.Adapter<Adaptador.JugadorViewHolder>
             txtequipo = (TextView) itemView.findViewById(R.id.txtequipo);
             txtjugador.setTypeface(font);
             txtequipo.setTypeface(font);
+        }
+
+    }
+
+    public static class Vista2ViewHolder extends RecyclerView.ViewHolder {
+        NetworkImageView imgjugador2;
+        TextView txtjugador2;
+        TextView txtequipo2;
+
+        public Vista2ViewHolder(View itemView) {
+            super(itemView);
+            Typeface font = Typeface.createFromAsset(itemView.getContext().getAssets(), "fonts/Roboto-Regular.ttf");
+            imgjugador2 = (NetworkImageView) itemView.findViewById(R.id.imgjugador2);
+            txtjugador2 = (TextView) itemView.findViewById(R.id.txtjugador2);
+            txtequipo2 = (TextView) itemView.findViewById(R.id.txtequipo2);
+            txtjugador2.setTypeface(font);
+            txtequipo2.setTypeface(font);
         }
 
     }
